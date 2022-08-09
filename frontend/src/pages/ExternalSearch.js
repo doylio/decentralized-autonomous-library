498ff00d9c7332cea252263764b67128ba06f83f
Michael Free <michael.d.free@gmail.com>
2022-08-09 18:45:22 -0400

    basic frontend routing setup

---
+++ frontend/src/pages/ExternalSearch.js
@@ -0,0 +1,5 @@
+const ExternalSearch = () => {
+    return <h1>External Search</h1>;
+  };
+  
+  export default ExternalSearch;
---
+++ frontend/src/pages/InternalSearch.js
@@ -0,0 +1,6 @@
+
+const InternalSearch = () => {
+    return <h1>Internal Search</h1>;
+  };
+  
+  export default InternalSearch;
