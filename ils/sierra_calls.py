559545e5865122dcd9491eaac6fc233310a28a06
Michael Free <michael.d.free@gmail.com>
2022-08-09 17:08:26 -0400

    cleanup of comments. creating seperate funtions for each arg passed to ils app

--- ils/sierra_calls.py
+++ ils/sierra_calls.py
@@ -10,6 +10,19 @@ from auth import sierra_auth
 from patron import patron_session, patron_verify
 from item import item_session
 
+def create_ilsitem(new_id, location_id, status_code, bar_code, item_title, author_title):
+
+    return
+
+def search_itemno():
+    return
+
+def search_author():
+    return
+
+def search_title():
+    return
+
 def get_patroninfo(patron_urls, patron_header, patron_ids):
     '''
     get patron info
@@ -33,50 +46,31 @@ def get_itemdata(json_file):
     return item_list
 
 if __name__ == '__main__':
-    # get the current directory
     current_dir = dirname(realpath(__file__))
-    # import the configuration file
     config_file = ConfigParser()
     config_file.read(current_dir+"/config.ini")
     sierra_host = config_file['sierra']['api_host']
     sierra_uri = config_file['sierra']['api_uri']
     sierra_key = config_file['sierra']['api_key']
     sierra_secret = config_file['sierra']['api_secret']
-    # form url of sierra api
     sierra_addr = sierra_host+sierra_uri
-    # make SIERRA_AUTH a var to call later
     access_token = sierra_auth(sierra_addr, sierra_key, sierra_secret)
-    # ils database
     ils_db = current_dir+'/books.json'
-    # MAKE SURE WE GET AN OK REPONSE FROM THE SIERRA SERVER
     if access_token != 'BAD_RESPONSE':
-         # patron calls made here
         item_url = item_session(sierra_addr,access_token)[0]
-        # headers to make the call to patron api
         item_headers = item_session(sierra_addr,access_token)[1]
         if sys.argv[1] == 'createitem':
-            # second argument is json data file
             print(sys.argv[2])
-            #get the key values of each json object
             item_metadata = get_itemdata(sys.argv[2])
-            #watcher item info (books)
             watcher_data = current_dir+'/watch_info.json'
-            #check and see if watcher_into.json file exists
             if bool(os.path.isfile(watcher_data)):
-                #if it does - append to that file
                 print(item_metadata)
             else:
-                #if it doesn't create the file
                 print(item_metadata)
         if sys.argv[1] == 'patron':
-            # second argument is a library card number
             patron_id = sys.argv[2]
-            # url to make the call to initiate session to ILS API
             patron_url = patron_session(sierra_addr,access_token)[0] # patron calls made here
-            # form the headers for the session to make calls ot the api
             patron_headers = patron_session(sierra_addr,access_token)[1]
-            #verify if the patron exists and get info
-            # if doesn't equal flase - write to file
             print(get_patroninfo(patron_url, patron_headers, patron_id))
 
         if sys.argv[1] == 'searchitem':
@@ -106,9 +100,10 @@ if __name__ == '__main__':
                         item_number = find_itemno[key]
                         if item_number['barcode'] == itemno_provided:
                             print(item_number)
+                        #else return nothing
 
         if sys.argv[1] == 'checkoutitem':
-            checkout_itme = sys.argv[2]
+            checkout_item = sys.argv[2]
             print("asdf") #change state
 
         if sys.argv[1] == 'checkinitem':
