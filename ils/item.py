b4d2c329c64f97d21c0874194cfda9a9e02e846e
Michael Free <michael.d.free@gmail.com>
2022-08-09 12:02:13 -0400

    beginning item api work

---
+++ ils/item.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+def item_session(host_uri, session_token):
+    # add the /item to host uri
+    item_url = host_uri+'/items'
+    item_headers = {'Accept': 'application/json', 'Authorization': 'Bearer ' + session_token,'ContentType':'application/json'}
+    return item_url, item_headers
+
+def item_verify():
+  return
+
+def item_data():
+  return
+
+def item_checkout():
+  return
+
+def item_return():
+  return
+
+def item_create(item_json, item_uri, item_headers):
+    #item_object = json.load(item_json)
+    item_objects = open(item_json)
+    item_data = json.load(item_objects)
+    for (v) in item_data.values():
+      #item uri 
+      print(v)
+    #patron_verified = requests.get(patron_uri+'/'+patron_id, headers = patron_headers)
+    #if patron_verified.status_code == 200:
+    #    patron_status = patron_verified.text
+    #else:
+    #    print(patron_verified.status_code)
+    #    print(patron_verified.text)
+    #    patron_status = False
+    #return patron_status
+    return item_json
+
+
+'''items = '/home/mfree@cambridgelibraries.ca/Documents/Repositories/hedera-library-system/data.json'
+item_uri = '/v6/items'
+item_create(items, item_uri )'''
--- ils/sierra_calls.py
+++ ils/sierra_calls.py
@@ -1,8 +1,9 @@
 from configparser import ConfigParser
 from os.path import dirname, realpath
-from sierra_modules.auth import sierra_auth
-from sierra_modules.patron import patron_session, patron_verify
-
+from auth import sierra_auth
+from patron import patron_session, patron_verify
+from item import item_session
+import sys
 def read_config():
     return
 
@@ -25,13 +26,22 @@ if __name__ == '__main__':
     access_token = sierra_auth(sierra_addr, sierra_key, sierra_secret)
     # MAKE SURE WE GET AN OK REPONSE FROM THE SIERRA SERVER
     if access_token != 'BAD_RESPONSE':
-        
-        patron_url = patron_session(sierra_addr,access_token)[0] # patron calls made here
-        patron_headers = patron_session(sierra_addr,access_token)[1] # headers to make the call to patron api
-        patron_id = patron_verify(patron_url, patron_headers, '1234567') 
+
+        item_url = item_session(sierra_addr,access_token)[0] # patron calls made here
+        item_headers = item_session(sierra_addr,access_token)[1] # headers to make the call to patron api
+        print(sys.argv)
+        #if create time is flagged
+        #if sys.argv[0] == "--create-item":
+            # get the second argument, which is a json file.
+        #    item_data = sys.argv[1]
+        #    print(item_data)
+            #for loop - loop through the file 
+            #for each item in the file - call, create item
+            #create_item()
+        #patron_id = patron_verify(patron_url, patron_headers, '1234567') 
         # if valid - return library card number, and patron type.
-        print(patron_id)
-        # need to work on item logic
+        #print(patron_id)
+
     else:
         print('COULDN\'T AUTHENTICATE')
         print(access_token)
