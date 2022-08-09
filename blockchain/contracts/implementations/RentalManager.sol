e3fd2911a8b188d7932188fd885f28358d6586a2
doylio <shawndoyle@Shawns-MacBook-Pro-220.local>
2022-08-09 15:13:07 -0400

    Finish rental manager

--- blockchain/contracts/implementations/RentalManager.sol
+++ blockchain/contracts/implementations/RentalManager.sol
@@ -22,8 +22,6 @@ contract RentalManager {
     enum RentalStatus {
         Offered,
         Accepted,
-        ShippedTo,
-        ShippedBack,
         Rejected,
         Returned
     }
@@ -33,26 +31,11 @@ contract RentalManager {
         uint256 quantity;
         uint256 bond;
         uint256 fee;
-        uint256 duration; // In days
-        uint256 expires;
         RentalStatus status;
     }
-    event RentalCreated(
-        uint256 id,
-        uint256 requestId,
-        address loaner,
-        uint256 quantity,
-        uint256 bond,
-        uint256 fee,
-        uint256 duration,
-        uint256 expires
-    );
+    event RentalCreated(uint256 id, uint256 requestId, address loaner, uint256 quantity, uint256 bond, uint256 fee);
 
     event RentalAccepted(uint256 id);
-
-    event RentalShippedTo(uint256 id, string verificationHash);
-
-    event RentalShippedBack(uint256 id, string verificationHash);
 
     event RentalReturned(uint256 id);
 
@@ -72,9 +55,7 @@ contract RentalManager {
         uint256 _requestId,
         uint256 _quantity,
         uint256 _bond,
-        uint256 _fee,
-        uint256 _duration,
-        uint256 _expires
+        uint256 _fee
     ) external {
         require(_requestId < requestCount, "RentalManager: request with this id does not exist");
         Request memory request = requests[_requestId];
@@ -83,17 +64,8 @@ contract RentalManager {
             "RentalManager: not enough books in catalogue"
         );
         uint256 rentalId = rentalCount++;
-        rentals[rentalId] = Rental(
-            _requestId,
-            msg.sender,
-            _quantity,
-            _bond,
-            _fee,
-            _duration,
-            _expires,
-            RentalStatus.Offered
-        );
-        emit RentalCreated(rentalId, _requestId, msg.sender, _quantity, _bond, _fee, _duration, _expires);
+        rentals[rentalId] = Rental(_requestId, msg.sender, _quantity, _bond, _fee, RentalStatus.Offered);
+        emit RentalCreated(rentalId, _requestId, msg.sender, _quantity, _bond, _fee);
     }
 
     function acceptOffer(uint256 _rentalId) external {
@@ -101,51 +73,31 @@ contract RentalManager {
 
         _verifyRenter(request);
         require(rental.status == RentalStatus.Offered, "RentalManager: rental is not offered");
-        require(rental.expires < block.timestamp, "RentalManager: this offer is expired");
 
         rentals[_rentalId].status = RentalStatus.Accepted;
 
         // transfer the fee & bond to this contract
         IERC20(paymentToken).transferFrom(request.renter, address(this), rental.fee + rental.bond);
 
+        ICatalogue(catalogue).safeTransferFrom(rental.loaner, request.renter, request.ISBN, rental.quantity, "");
+
         emit RentalAccepted(_rentalId);
     }
 
-    function markShippedTo(uint256 _rentalId, string calldata _verificationHash) external {
+    function rentalReturned(uint256 _rentalId) external {
         (Request memory request, Rental memory rental) = _getRequestAndRental(_rentalId);
         _verifyLoaner(rental);
 
         require(rental.status == RentalStatus.Accepted, "RentalManager: rental is not accepted");
 
-        rentals[_rentalId].status = RentalStatus.ShippedTo;
-
-        ICatalogue(catalogue).safeTransferFrom(rental.loaner, request.renter, request.ISBN, rental.quantity, "");
-
-        emit RentalShippedTo(_rentalId, _verificationHash);
-    }
-
-    function markShippedBack(uint256 _rentalId, string calldata _verificationHash) external {
-        (Request memory request, Rental memory rental) = _getRequestAndRental(_rentalId);
-        _verifyRenter(request);
-
-        require(rental.status == RentalStatus.ShippedBack, "RentalManager: rental has not been shipped from owner");
-
-        rentals[_rentalId].status = RentalStatus.ShippedBack;
-
-        ICatalogue(catalogue).safeTransferFrom(request.renter, rental.loaner, request.ISBN, rental.quantity, "");
-
-        emit RentalShippedBack(_rentalId, _verificationHash);
-    }
-
-    function rentalReturned(uint256 _rentalId) external {
-        require(_rentalId < rentalCount, "RentalManager: rental with this id does not exist");
-        Rental memory rental = rentals[_rentalId];
-        require(rental.status == RentalStatus.Accepted, "RentalManager: rental is not accepted");
-        require(rental.loaner == msg.sender, "RentalManager: you are not the loaner");
-
         rentals[_rentalId].status = RentalStatus.Returned;
 
-        // Return
+        // Return the bond
+        IERC20(paymentToken).transferFrom(address(this), request.renter, rental.bond);
+        // Pay the fee
+        IERC20(paymentToken).transferFrom(address(this), rental.loaner, rental.fee);
+        // Return the token
+        ICatalogue(catalogue).safeTransferFrom(request.renter, rental.loaner, request.ISBN, rental.quantity, "");
 
         emit RentalReturned(_rentalId);
     }
