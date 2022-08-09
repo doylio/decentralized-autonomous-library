8c6d94abff8790a4d254f7efcb88e790d61df119
Michael Free <michael.d.free@gmail.com>
2022-08-09 14:42:08 -0400

    new cli functions for seraching items started

--- ils/sierra_calls.py
+++ ils/sierra_calls.py
@@ -4,11 +4,16 @@ from auth import sierra_auth
 from patron import patron_session, patron_verify
 from item import item_session
 import sys
+import os.path
 import json
 
 def get_patroninfo(patron_url, patron_headers, patron_id):
-    patron_json = patron_verify(patron_url, patron_headers, patron_id) 
-    return patron_json
+    patron_json = patron_verify(patron_url, patron_headers, patron_id)
+    if patron_json == False:
+        member_status = False
+    else:
+        member_status = patron_json
+    return member_status
 
 def get_itemdata(json_file):
     item_objects = open(json_file)
@@ -41,14 +46,28 @@ if __name__ == '__main__':
             print(sys.argv[2])
             #get the key values of each json object
             item_metadata = get_itemdata(sys.argv[2])
-            print(item_metadata)
+            #watcher item info (books)
+            watcher_data = current_dir+'/watch_info.json'
+            #check and see if watcher_into.json file exists
+            if bool(os.path.isfile(watcher_data)):
+                #if it does - append to that file
+                print(item_metadata)
+            else:
+                #if it doesn't create the file
+                print("hello!")
         if sys.argv[1] == 'patron':
-            print(sys.argv[2])
             # second argument is a library card number
             patron_id = sys.argv[2]
+            # url to make the call to initiate session to ILS API
             patron_url = patron_session(sierra_addr,access_token)[0] # patron calls made here
-            patron_headers = patron_session(sierra_addr,access_token)[1] # headers to make the call
+            # form the headers for the session to make calls ot the api
+            patron_headers = patron_session(sierra_addr,access_token)[1]
+            #verify if the patron exists and get info
+            # if doesn't equal flase - write to file
             print(get_patroninfo(patron_url, patron_headers, patron_id))
+        if sys.argv[1] == 'searchitem':
+            find_itemid = sys.argv[2]
+
     else:
         print('COULDN\'T AUTHENTICATE')
         print(access_token)
