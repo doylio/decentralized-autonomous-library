09dcb8e111c5fae5bcd242cc7e7bcac63f9814fb
isaac pontarelli <ipontarelli@uri.edu>
2022-08-10 00:19:14 -0400

    simulated ws by polling endpoint

--- watcher/pkg/listener/types.go
+++ watcher/pkg/listener/types.go
@@ -1,6 +1,33 @@
 package listener
 
+import (
+	"math/big"
+	"sync"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
+type State struct {
+	RequestsCreated map[string]RequestCreated
+	RentalsCreated  map[string]RentalCreated
+	RentalsAccepted map[string]RentalAccepted
+	Lock            sync.RWMutex
+}
+
 type RequestCreated struct {
+	ID       *big.Int
+	Renter   common.Address
+	ISBN     *big.Int
+	Quantity *big.Int
+}
+
+type RentalCreated struct {
+	ID        *big.Int
+	RequestID *big.Int
+	Loaner    common.Address
+	Quantity  *big.Int
+	Bond      *big.Int
+	Fee       *big.Int
 }
 
 type RentalAccepted struct {
