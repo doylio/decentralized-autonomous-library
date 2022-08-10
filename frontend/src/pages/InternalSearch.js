d2dc52cf284aac09d4d52ab3132387bb1b4d1eff
Michael Free <michael.d.free@gmail.com>
2022-08-09 20:03:49 -0400

    starting to get form data for internal search

--- frontend/src/pages/InternalSearch.js
+++ frontend/src/pages/InternalSearch.js
@@ -1,20 +1,31 @@
-
+import React from "react";
+import { useState } from 'react';
+import ReactDOM from "react-dom/client";
 
 
 const InternalSearch = () => {
+  const [barcode, setBarcode] = useState("")
+  const [itemtitle, setItemTitle] = useState("")
+  const [authorname, setAuthorName] = useState("")
+  console.log(barcode)
+  console.log(itemtitle)
+  console.log(authorname)
     return (
     <div><h1>Internal Search</h1>
     <br></br>
     <label for="barcode">Barcode : </label>
-      <input type="text" id="barcode" name="barcode"></input>
+      <input type="text" id="barcode" name="barcode" value={barcode} onChange={ev=>setBarcode(ev.target.value)}></input>
       <br></br>
       <label for="item_title">Title : </label>
-      <input type="text" id="item_title" name="item_title"></input>
+      <input type="text" id="itemtitle" name="itemtitle" value={itemtitle} onChange={ev=>setItemTitle(ev.target.value)}></input>
       <br></br>
-      <label for="author_name">Author : </label>
-      <input type="text" id="author_name" name="author_name"></input>
+      <label for="authorname">Author : </label>
+      <input type="text" id="authorname" name="authorname" value={authorname} onChange={ev=>setAuthorName(ev.target.value)}></input>
       <br></br>
-      <input type="submit" value="Search"></input>
+      <button
+        type="button"
+        onClick={setBarcode} //function to go to page results page
+      >Search</button>
     </div>
     
     );
