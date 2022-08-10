d266fb753c99b0fef5681fb8dc14dd738fdd5bce
doylio <shawndoyle@Shawns-MacBook-Pro-220.local>
2022-08-09 22:12:29 -0400

    start accept offer function

--- frontend/src/pages/ViewOffers.js
+++ frontend/src/pages/ViewOffers.js
@@ -1,9 +1,24 @@
+import { useSigner } from "wagmi";
+
 const ViewOffers = () => {
-    return (
+  const { data: signer } = useSigner();
+
+  async function acceptOffer(offerId) {
+    try {
+      const rentalManager = getRentalManager();
+      const tx = await rentalManager.connect(signer).acceptOffer(offerId);
+      await tx.wait();
+      window.location.reload();
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  return (
     <div>
-    <h1>View Available Books for Offer</h1>
+      <h1>View Available Books for Offer</h1>
     </div>
-    );
-  };
-  
-  export default ViewOffers;
+  );
+};
+
+export default ViewOffers;
