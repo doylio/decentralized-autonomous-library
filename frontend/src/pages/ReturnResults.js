fb02e5909448b50c932ff55d9e76eef3174ff5a0
Michael Free <michael.d.free@gmail.com>
2022-08-09 23:20:47 -0400

    fixing spelling mistake

--- frontend/src/pages/ReturnResults.js
+++ frontend/src/pages/ReturnResults.js
@@ -6,13 +6,17 @@ class bookAuthor extends React.Component {
     const apiAuthorUrl = "http://localhost:8080/bookByAuthor";
     const apiBarcodeUrl = "http://localhost:8080/bookByBarcode";
     const apiTitleUrl = "http://localhost:8080/bookByTitle";
-
-    fetch(apiAuthorUrl)
+    fetch(apiAuthorUrl+"/"+qs.get(bookAuthor))
       .then((response) => response.json())
       .then((data) => console.log("This is your data", data));
   }
   render() {
-    return <h1>my Component has Mounted, Check the browser 'console' </h1>;
+    return (
+    <div>
+    <h1>my Component has Mounted, Check the browser 'console' </h1>
+    
+    </div>
+    );
   }
 }
 export default bookAuthor;
