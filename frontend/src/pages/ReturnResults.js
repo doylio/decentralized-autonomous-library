ee3adf034eda9c412a3219813828f3bdc6a15753
Michael Free <michael.d.free@gmail.com>
2022-08-09 22:17:35 -0400

    bringing it all into one component

--- frontend/src/pages/ReturnResults.js
+++ frontend/src/pages/ReturnResults.js
@@ -3,6 +3,10 @@ import React from 'react';
 class bookAuthor extends React.Component {
   componentDidMount() {
     const apiAuthorUrl = 'http://localhost:8080/bookByAuthor';
+    const apiBarcodeUrl = 'http://localhost:8080/bookByBarcode';
+    const apiTitleUrl = 'http://localhost:8080/bookByTitle';
+
+
     fetch(apiAuthorUrl)
       .then((response) => response.json())
       .then((data) => console.log('This is your data', data));
@@ -13,32 +17,6 @@ class bookAuthor extends React.Component {
 }
 export default bookAuthor;
 
-
-class bookBarcode extends React.Component {
-  componentDidMount() {
-    const apiBarcodeUrl = 'http://localhost:8080/bookByBarcode';
-    fetch(apiBarcodeUrl)
-      .then((response) => response.json())
-      .then((data) => console.log('This is your data', data));
-  }
-  render() {
-    return <h1>my Component has Mounted, Check the browser 'console' </h1>;
-  }
-}
-export default bookBarcode;
-
-class bookTitle extends React.Component {
-  componentDidMount() {
-    const apiTitleUrl = 'http://localhost:8080/bookByTitle';
-    fetch(apiTitleUrl)
-      .then((response) => response.json())
-      .then((data) => console.log('This is your data', data));
-  }
-  render() {
-    return <h1>my Component has Mounted, Check the browser 'console' </h1>;
-  }
-}
-export default bookTitle
 /*
 const ReturnResults = () => {
     return (
