a37f9f1c9c56650c05486802ac30d6db0defd583
Michael Free <michael.d.free@gmail.com>
2022-08-09 22:11:01 -0400

    building in uris for book apis and returning them

--- frontend/src/pages/ReturnResults.js
+++ frontend/src/pages/ReturnResults.js
@@ -1,9 +1,53 @@
+import React from 'react';
+
+class bookAuthor extends React.Component {
+  componentDidMount() {
+    const apiAuthorUrl = 'http://localhost:8080/bookByAuthor';
+    fetch(apiAuthorUrl)
+      .then((response) => response.json())
+      .then((data) => console.log('This is your data', data));
+  }
+  render() {
+    return <h1>my Component has Mounted, Check the browser 'console' </h1>;
+  }
+}
+export default bookAuthor;
+
+
+class bookBarcode extends React.Component {
+  componentDidMount() {
+    const apiBarcodeUrl = 'http://localhost:8080/bookByBarcode';
+    fetch(apiBarcodeUrl)
+      .then((response) => response.json())
+      .then((data) => console.log('This is your data', data));
+  }
+  render() {
+    return <h1>my Component has Mounted, Check the browser 'console' </h1>;
+  }
+}
+export default bookBarcode;
+
+class bookTitle extends React.Component {
+  componentDidMount() {
+    const apiTitleUrl = 'http://localhost:8080/bookByTitle';
+    fetch(apiTitleUrl)
+      .then((response) => response.json())
+      .then((data) => console.log('This is your data', data));
+  }
+  render() {
+    return <h1>my Component has Mounted, Check the browser 'console' </h1>;
+  }
+}
+export default bookTitle
+/*
 const ReturnResults = () => {
     return (
     <div>
     <h1>Return Results</h1>
+
     </div>
     );
   };
   
   export default ReturnResults;
+  */
